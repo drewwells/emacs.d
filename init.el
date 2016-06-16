0fd7c738ac2ca990fd37938ed4b0139cecdc9380
Drew Wells <drew.wells00@gmail.com>
2016-06-16 18:43:14 -0500

    check in config settings

---
+++ init.el
@@ -0,0 +1,55 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(global-whitespace-mode nil)
+ '(package-archives
+   (quote
+    (("melpa" . "https://melpa.org/packages/")
+     ("gnu" . "http://elpa.gnu.org/packages/"))))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "nil" :slant normal :weight normal :height 181 :width normal)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; List of manual things to load ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; force package initialization
+;; http://stackoverflow.com/questions/24610945/emacs-cant-autostart-projectile-installed-through-melpa
+(require 'package)
+(setq package-enable-at-startup nil) ; To avoid initializing twice
+(package-initialize)
+
+(load-theme 'solarized-dark)
+
+(projectile-global-mode)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; go stuff https://godoc.org/golang.org/x/tools/cmd/goimports
+(setq gofmt-command "goimports")
+;;(add-to-list 'load-path "/home/you/somewhere/emacs/")
+;;(require 'go-mode-load)
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+;; go-autocomplete https://github.com/nsf/gocode
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; go-oracle https://docs.google.com/document/d/1SLk36YRjjMgKqe490mSRzOPYEDe0Y_WQNRv-EiFYUyw/view?pli=1#
+(load "~/src/golang.org/x/tools/cmd/oracle/oracle.el")
+
+;; http://stackoverflow.com/questions/69934/set-4-space-indent-in-emacs-in-text-mode
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
