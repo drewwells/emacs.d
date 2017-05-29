4421f983e2c74d612e81391acf885f2a759bef17
Drew Wells <drew.wells00@gmail.com>
2017-05-28 23:49:48 -0500

    upgrade packages

---
+++ elpa/browse-at-remote-20170523.556/browse-at-remote-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "browse-at-remote" "20170523.556" "Open github/gitlab/bitbucket/stash page from Emacs" '((f "0.17.2") (s "1.9.0") (cl-lib "0.5")) :commit "5de73fcaa54b638554fcbd59a693bc647ff9dee1" :keywords '("github" "gitlab" "bitbucket" "convenience"))
