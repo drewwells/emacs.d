f06db03a0b971267275671d0700b6a6c82e75f68
Drew Wells <drew.wells00@gmail.com>
2017-11-07 10:49:25 -0600

    update packages

--- elpa/projectile-20170725.1304/projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "projectile" "20170725.1304" "Manage and navigate projects in Emacs easily" '((emacs "24.1") (pkg-info "0.4")) :commit "63dc5bcf0803ef81bc13ca499e60c61b8fea2fd6" :url "https://github.com/bbatsov/projectile" :keywords '("project" "convenience"))
