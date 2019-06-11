07b9cacc238d64f75b39f9803de585f4c841a405
Drew Wells <drew.wells00@gmail.com>
2019-06-11 18:11:19 -0500

    smerge

--- init.el
+++ init.el
@@ -190,6 +190,17 @@
 
 (add-to-list 'auto-mode-alist '("\\.ino\\'" . c++-mode))
 
+;; https://emacs.stackexchange.com/questions/16469/how-to-merge-git-conflicts-in-emacs
+;; use smerge when opening conflicted files
+(defun my-enable-smerge-maybe ()
+  (when (and buffer-file-name (vc-backend buffer-file-name))
+    (save-excursion
+      (goto-char (point-min))
+      (when (re-search-forward "^<<<<<<< " nil t)
+        (smerge-mode +1)))))
+
+(add-hook 'buffer-list-update-hook #'my-enable-smerge-maybe)
+
 ;; oracle proxy
 ;; (setq url-proxy-services
 ;;       '(("no_proxy" . "^\\(localhost\\|10.*\\)")
