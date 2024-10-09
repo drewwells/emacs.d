c3f9f7b49471e45859ea9135914d7542737005e9
Drew Wells <dwells@infoblox.com>
2024-10-08 19:05:35 -0500

    updates

---
+++ elpa/editorconfig-20240813.801/editorconfig-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "editorconfig" "20240813.801"
+  "EditorConfig Emacs Plugin."
+  '((emacs "26.1"))
+  :url "https://github.com/editorconfig/editorconfig-emacs"
+  :commit "648f0cf9aeb72db77b252832a58367332b7bc055"
+  :revdesc "648f0cf9aeb7"
+  :keywords '("convenience" "editorconfig")
+  :authors '(("EditorConfig Team" . "editorconfig@googlegroups.com"))
+  :maintainers '(("EditorConfig Team" . "editorconfig@googlegroups.com")))
--- init.el
+++ init.el
@@ -11,11 +11,9 @@
  '(create-lockfiles nil)
  '(current-language-environment "ASCII")
  '(custom-safe-themes
-   '("51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "13a8eaddb003fd0d561096e11e1a91b029d3c9d64554f8e897b2513dbf14b277" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
- '(custom-theme-directory "~/.emacs.d/themes/emacs-color-theme-solarized/")
+   '("fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "13a8eaddb003fd0d561096e11e1a91b029d3c9d64554f8e897b2513dbf14b277" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
  '(exec-path
    '("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/usr/local/bin" "~/bin" "/usr/local/go/bin" "~/.cargo/bin"))
- '(frame-background-mode 'dark)
  '(global-whitespace-mode nil)
  '(go-guru-build-tags "")
  '(go-guru-debug t)
@@ -26,11 +24,10 @@
  '(jenkinsfile-mode-indent-offset 2)
  '(js-indent-level 2)
  '(menu-bar-mode nil)
- '(mode-require-final-newline nil)
  '(nxml-child-indent 2 t)
  '(package-enable-at-startup nil)
  '(package-selected-packages
-   '(flycheck-rust rust-mode ethan-wspace copilot flymake-go-staticcheck json-rpc editorconfig quelpa-use-package quelpa flymake flymake-go eglot jenkinsfile-mode company go-mode yasnippet use-package flycheck exec-path-from-shell yaml-mode protobuf-mode smart-tabs-mode helm helm-ag helm-projectile helm-pt magit cl-lib popup flx-ido browse-at-remote ag))
+   '(solarized-theme flycheck-rust rust-mode ethan-wspace copilot flymake-go-staticcheck json-rpc editorconfig quelpa-use-package quelpa flymake flymake-go eglot jenkinsfile-mode company go-mode yasnippet use-package flycheck exec-path-from-shell yaml-mode protobuf-mode smart-tabs-mode helm helm-ag helm-projectile helm-pt magit cl-lib popup flx-ido browse-at-remote ag))
  '(projectile-completion-system 'helm)
  '(projectile-enable-caching t)
  '(projectile-globally-ignored-directories
@@ -45,6 +42,11 @@
 
 ;; https://github.com/Wilfred/ag.el/issues/93#issuecomment-348003505
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+
+;; CLI tools installed by Mise
+;; https://mise.jdx.dev/ide-integration.html#emacs
+(setenv "PATH" (concat (getenv "PATH") ":~/.local/share/mise/shims"))
+(setq exec-path (append exec-path '("~/.local/share/mise/shims")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; List of manual things to load ;;
@@ -86,9 +88,9 @@
 
 ;; disabled, conflicts with pls-mode
 ;; go stuff https://godoc.org/golang.org/x/tools/cmd/goimports
-;; (setq gofmt-command "goimports")
+(setq gofmt-command "goimports")
 
-;; (add-hook 'before-save-hook 'gofmt-before-save)
+(add-hook 'before-save-hook 'gofmt-before-save)
 
 ;; http://stackoverflow.com/questions/69934/set-4-space-indent-in-emacs-in-text-mode
 (setq-default indent-tabs-mode nil)
@@ -224,9 +226,6 @@
 ;; you can utilize :map :hook and :config to customize copilot
 (add-hook 'prog-mode-hook 'copilot-mode)
 
-;;(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
-;;(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -234,70 +233,10 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; https://github.com/sellout/emacs-color-theme-solarized#installation--usage
-(add-to-list 'load-path "~/.emacs.d/themes/emacs-color-theme-solarized/")
-;;(load-theme 'solarized t)
-;; ChatGPT manual application of fonts
-
-(defun my-solarized-light-colors ()
-  (set-face-attribute 'default nil :foreground "#839496") ; Default text color (base0)
-  (set-face-foreground 'font-lock-comment-face "#586e75") ; Comments (base01)
-  (set-face-foreground 'font-lock-string-face "#2aa198")  ; Strings (cyan)
-  (set-face-foreground 'font-lock-keyword-face "#859900") ; Keywords (green)
-  (set-face-foreground 'font-lock-function-name-face "#268bd2") ; Function names (blue)
-  (set-face-foreground 'font-lock-variable-name-face "#b58900") ; Variables (yellow)
-  (set-face-foreground 'font-lock-type-face "#268bd2") ; Types (blue)
-  (set-face-foreground 'font-lock-constant-face "#d33682") ; Constants (magenta)
-  (set-face-foreground 'font-lock-builtin-face "#6c71c4") ; Built-in functions (violet)
-  (set-face-foreground 'font-lock-warning-face "#dc322f")) ; Warnings (red)
-
-(defun my-solarized-dark-colors ()
-  (set-face-attribute 'default nil :foreground "#839496") ; Default text color (base0)
-  (set-face-foreground 'font-lock-comment-face "#93a1a1") ; Comments (base01)
-  (set-face-foreground 'font-lock-string-face "#2aa198")  ; Strings (cyan)
-  (set-face-foreground 'font-lock-keyword-face "#b58900") ; Keywords (yellow)
-  (set-face-foreground 'font-lock-function-name-face "#268bd2") ; Function names (blue)
-  (set-face-foreground 'font-lock-variable-name-face "#859900") ; Variables (green)
-  (set-face-foreground 'font-lock-type-face "#b58900") ; Types (yellow)
-  (set-face-foreground 'font-lock-constant-face "#6c71c4") ; Constants (violet)
-  (set-face-foreground 'font-lock-builtin-face "#cb4b16") ; Built-in functions (orange)
-  (set-face-foreground 'font-lock-warning-face "#dc322f")) ; Warnings (red)
-
-(defun my-toggle-solarized-colors ()
-  (if (eq frame-background-mode 'dark)
-      (my-solarized-dark-colors)
-    (my-solarized-light-colors)))
-
-;; Hook this function to be called whenever the frame background mode changes
-(add-hook 'after-make-frame-functions (lambda (frame)
-                                        (with-selected-frame frame
-                                          (my-toggle-solarized-colors))))
-
-;; Call the function immediately to apply the colors to the current frame
-(my-toggle-solarized-colors)
-
-(add-hook 'emacs-startup-hook 'my-toggle-solarized-colors)
-
-(defun my-toggle-frame-background-mode ()
-  (interactive)
-  (if (eq frame-background-mode 'dark)
-      (progn
-        (setq frame-background-mode 'light)
-        (my-solarized-light-colors))
-    (progn
-      (setq frame-background-mode 'dark)
-      (my-solarized-dark-colors)))
-  (frame-set-background-mode nil))  ; Apply the new mode
-
-(global-set-key (kbd "C-c t") 'my-toggle-frame-background-mode) ; Bind to a key, e.g., Ctrl-c t
-
-
+(load-theme 'solarized-light t)
 
 ;; Editorconfig
 (editorconfig-mode 1)
-
-;; (add-hook 'go-mode-hook #'flymake-go-staticcheck-enable)
-;; (add-hook 'go-mode-hook #'flymake-mode)
 
 ;; Chatgpt recommendations for invalidating projectile cache
 ;;(add-hook 'after-save-hook 'projectile-invalidate-cache nil t)
